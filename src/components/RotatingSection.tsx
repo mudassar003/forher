094cf51179d35f88fdac93fe93306d2c381f2894
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2025-02-17 00:48:08 +0500

    Update

--- src/components/RotatingSection.tsx
+++ src/components/RotatingSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
 const rotatingTexts = [
   "Save 30% on avg. with FSA & HSA",

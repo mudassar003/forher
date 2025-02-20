2eae415fbdd81c4ee38b0ae067e24006245bad8a
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2025-02-20 22:29:27 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import HomeHeader from "@/components/HomeHeader";
 import GlobalFooter from "@/components/GlobalFooter";
 
 const geistSans = Geist({

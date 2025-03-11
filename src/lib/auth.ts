dc6b999aa51892bcfbbf9e6baa760827ae8d2aa8
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2025-03-11 15:33:40 +0500

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,7 +19,7 @@ export const signInWithGoogle = async (returnUrl?: string) => {
     return { error: error.message };
   }
 
-  return { user: data };
+  return { user: data.user, session: data.session };
 };
 
 /**
@@ -33,7 +33,7 @@ export const signUpWithEmail = async (email: string, password: string) => {
     return { error: error.message };
   }
 
-  return { user: data };
+  return { user: data.user, session: data.session };
 };
 
 /**
@@ -50,7 +50,7 @@ export const signInWithEmail = async (email: string, password: string) => {
     return { error: error.message };
   }
 
-  return { user: data };
+  return { user: data.user, session: data.session };
 };
 
 /**

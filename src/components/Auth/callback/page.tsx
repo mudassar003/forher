53757e43c14df062c632539de53a5451b8c93238
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2025-03-11 17:14:51 +0500

    update

--- src/components/Auth/callback/page.tsx
+++
@@ -1,7 +0,0 @@
-//src/app/auth/callback/page.tsx
-"use client";
-import AuthCallback from "@/components/Auth/AuthCallback";
-
-export default function AuthCallbackPage() {
-  return <AuthCallback />;
-}

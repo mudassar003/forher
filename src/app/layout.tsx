888f06b8362545acfcdc037da4844a96465d05c1
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2025-04-05 21:39:03 +0500

    Title Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "<div>Lily&apos;s</div>",
+  title: "Lily's",
   description: "Start your journey with personalized care",
 };
 
